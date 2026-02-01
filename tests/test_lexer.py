eb635fbe9925a5963cf3b483903c7e789e299933
Hoan Nguyen <nvhoanbk14@gmail.com>
2026-02-01 18:43:58 +0700

    Fix bug Struct, fix bug switch - không chặn nhiều default.

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -13,20 +13,24 @@ def test_keyword_auto():
     tokenizer = Tokenizer("auto")
     assert tokenizer.get_tokens_as_string() == "auto,<EOF>"
 
+
 def test_operator_assign():
     """2. Operator"""
     tokenizer = Tokenizer("=")
     assert tokenizer.get_tokens_as_string() == "=,<EOF>"
+
 
 def test_separator_semi():
     """3. Separator"""
     tokenizer = Tokenizer(";")
     assert tokenizer.get_tokens_as_string() == ";,<EOF>"
 
+
 def test_integer_single_digit():
     """4. Integer literal"""
     tokenizer = Tokenizer("5")
     assert tokenizer.get_tokens_as_string() == "5,<EOF>"
+
 
 def test_float_decimal():
     """5. Float literal"""
@@ -45,10 +49,12 @@ def test_identifier_simple():
     tokenizer = Tokenizer("x")
     assert tokenizer.get_tokens_as_string() == "x,<EOF>"
 
+
 def test_line_comment():
     """8. Line comment"""
     tokenizer = Tokenizer("// This is a comment")
     assert tokenizer.get_tokens_as_string() == "<EOF>"
+
 
 def test_block_comment():
     """9. Line comment"""
@@ -61,72 +67,92 @@ def test_integer_in_expression():
     tokenizer = Tokenizer("5+10")
     assert tokenizer.get_tokens_as_string() == "5,+,10,<EOF>"
 
+
 def test_11_plus_minus():
     """11. Mixed: plus and minus"""
     tokenizer = Tokenizer("5+10-15")
     assert tokenizer.get_tokens_as_string() == "5,+,10,-,15,<EOF>"
+
 
 def test_12_mul_div():
     """12. Mixed: multiplication and division"""
     tokenizer = Tokenizer("6*3/2")
     assert tokenizer.get_tokens_as_string() == "6,*,3,/,2,<EOF>"
 
+
 def test_13_mod():
     """13. Mixed: mod"""
     tokenizer = Tokenizer("10%3")
     assert tokenizer.get_tokens_as_string() == "10,%,3,<EOF>"
+
 
 def test_14_inc():
     """14. Mixed: increment"""
     tokenizer = Tokenizer("x++")
     assert tokenizer.get_tokens_as_string() == "x,++,<EOF>"
 
+
 def test_15_dec():
     """15. Mixed: decrement"""
-    tokenizer = Tokenizer("x--")
-    assert tokenizer.get_tokens_as_string() == "x,--,<EOF>"
+    tokenizer = Tokenizer("void main() { x = -++a--; }")
+    assert tokenizer.get_tokens_as_string() == "void,main,(,),{,x,=,-,++,a,--,;,},<EOF>"
+
 
 def test_16_equal():
     assert Tokenizer("a==b").get_tokens_as_string() == "a,==,b,<EOF>"
 
+
 def test_17_not_equal():
     assert Tokenizer("a!=b").get_tokens_as_string() == "a,!=,b,<EOF>"
+
 
 def test_18_and_or():
     assert Tokenizer("a&&b||c").get_tokens_as_string() == "a,&&,b,||,c,<EOF>"
 
+
 def test_19_relational():
     assert Tokenizer("a<=b").get_tokens_as_string() == "a,<=,b,<EOF>"
+
 
 def test_20_assign_chain():
     assert Tokenizer("a=b=c").get_tokens_as_string() == "a,=,b,=,c,<EOF>"
 
+
 def test_21_parentheses():
     assert Tokenizer("(a)").get_tokens_as_string() == "(,a,),<EOF>"
+
 
 def test_22_braces():
     assert Tokenizer("{ }").get_tokens_as_string() == "{,},<EOF>"
 
+
 def test_23_comma():
     assert Tokenizer("a,b,c").get_tokens_as_string() == "a,,,b,,,c,<EOF>"
+
 
 def test_24_dot():
     assert Tokenizer("p.x").get_tokens_as_string() == "p,.,x,<EOF>"
 
+
 def test_25_call_empty():
     assert Tokenizer("f()").get_tokens_as_string() == "f,(,),<EOF>"
+
 
 def test_26_call_args():
     assert Tokenizer("f(1,2)").get_tokens_as_string() == "f,(,1,,,2,),<EOF>"
 
+
 def test_27_nested_call():
     assert Tokenizer("f(g(x))").get_tokens_as_string() == "f,(,g,(,x,),),<EOF>"
+
 
 def test_28_block():
     assert Tokenizer("{x;}").get_tokens_as_string() == "{,x,;,},<EOF>"
 
+
 def test_29_colon():
     assert Tokenizer(":").get_tokens_as_string() == ":,<EOF>"
+
 
 def test_30_switch_case():
     assert Tokenizer("case 1:").get_tokens_as_string() == "case,1,:,<EOF>"
@@ -135,167 +161,301 @@ def test_30_switch_case():
 def test_31_if():
     assert Tokenizer("if").get_tokens_as_string() == "if,<EOF>"
 
+
 def test_32_else():
     assert Tokenizer("else").get_tokens_as_string() == "else,<EOF>"
+
 
 def test_33_for():
     assert Tokenizer("for").get_tokens_as_string() == "for,<EOF>"
 
+
 def test_34_while():
     assert Tokenizer("while").get_tokens_as_string() == "while,<EOF>"
+
 
 def test_35_switch():
     assert Tokenizer("switch").get_tokens_as_string() == "switch,<EOF>"
 
+
 def test_36_default():
     assert Tokenizer("default").get_tokens_as_string() == "default,<EOF>"
+
 
 def test_37_break():
     assert Tokenizer("break").get_tokens_as_string() == "break,<EOF>"
 
+
 def test_38_continue():
     assert Tokenizer("continue").get_tokens_as_string() == "continue,<EOF>"
+
 
 def test_39_return():
     assert Tokenizer("return").get_tokens_as_string() == "return,<EOF>"
 
+
 def test_40_struct():
     assert Tokenizer("struct").get_tokens_as_string() == "struct,<EOF>"
+
 
 def test_41_void():
     assert Tokenizer("void").get_tokens_as_string() == "void,<EOF>"
 
+
 def test_42_float_kw():
     assert Tokenizer("float").get_tokens_as_string() == "float,<EOF>"
+
 
 def test_43_string_kw():
     assert Tokenizer("string").get_tokens_as_string() == "string,<EOF>"
 
+
 def test_44_auto_decl():
     assert Tokenizer("auto x;").get_tokens_as_string() == "auto,x,;,<EOF>"
+
 
 def test_45_var_assign():
     assert Tokenizer("x=10;").get_tokens_as_string() == "x,=,10,;,<EOF>"
 
+
 def test_46_member_assign():
     assert Tokenizer("p.x=1;").get_tokens_as_string() == "p,.,x,=,1,;,<EOF>"
+
 
 def test_47_postfix_member():
     assert Tokenizer("p.x++;").get_tokens_as_string() == "p,.,x,++,;,<EOF>"
 
+
 def test_48_prefix_member():
     assert Tokenizer("++p.x;").get_tokens_as_string() == "++,p,.,x,;,<EOF>"
+
 
 def test_49_function_member():
     assert Tokenizer("print(p.x);").get_tokens_as_string() == "print,(,p,.,x,),;,<EOF>"
 
+
 def test_50_nested_member():
     assert Tokenizer("a.b.c").get_tokens_as_string() == "a,.,b,.,c,<EOF>"
+
 
 def test_51_empty_string():
     assert Tokenizer('""').get_tokens_as_string() == ",<EOF>"
 
+
 def test_52_tab_escape():
     assert Tokenizer('"a\\tb"').get_tokens_as_string() == "a\\tb,<EOF>"
+
 
 def test_53_newline_escape_illegal():
     assert Tokenizer('"a\nb"').get_tokens_as_string() == "Unclosed String: a"
 
+
 def test_54_quote_escape():
-    assert Tokenizer('"He said: \\"Hi\\""').get_tokens_as_string() == 'He said: \\"Hi\\",<EOF>'
+    assert (
+        Tokenizer('"He said: \\"Hi\\""').get_tokens_as_string()
+        == 'He said: \\"Hi\\",<EOF>'
+    )
+
 
 def test_55_backslash_escape():
     assert Tokenizer('"C:\\\\path"').get_tokens_as_string() == "C:\\\\path,<EOF>"
 
+
 def test_56_multiple_escapes():
     assert Tokenizer('"\\t\\n"').get_tokens_as_string() == "\\t\\n,<EOF>"
+
 
 def test_57_string_with_space():
     assert Tokenizer('"hello world"').get_tokens_as_string() == "hello world,<EOF>"
 
+
 def test_58_string_punctuation():
     assert Tokenizer('"a,b;c"').get_tokens_as_string() == "a,b;c,<EOF>"
+
 
 def test_59_string_in_expr():
     assert Tokenizer('print("x");').get_tokens_as_string() == "print,(,x,),;,<EOF>"
 
+
 def test_60_string_concat_like():
     assert Tokenizer('"a""b"').get_tokens_as_string() == "a,b,<EOF>"
 
+
+def test_61_string_with_single_quote():
+    assert Tokenizer("'a'").get_tokens_as_string() == "Error Token '"
+
+
+def test_62_unprintable_ascii():
+    assert Tokenizer('"\x01"').get_tokens_as_string() == "\x01,<EOF>"
+
+def test_63_id_case_sensitive():
+        assert Tokenizer("Var var VAR").get_tokens_as_string() == "Var,var,VAR,<EOF>"
+
+
+def test_64_int_zero():
+    assert Tokenizer("0").get_tokens_as_string() == "0,<EOF>"
+
+
+def test_65_negative_int():
+    assert Tokenizer("-123").get_tokens_as_string() == "-,123,<EOF>"
+
+
+def test_66_member_access_complex():
+    assert Tokenizer("a.b.c().d").get_tokens_as_string() == "a,.,b,.,c,(,),.,d,<EOF>"
+
+
+def test_67_multiple_lines():
+    assert (
+        Tokenizer("int x;\nfloat y;").get_tokens_as_string()
+        == "int,x,;,float,y,;,<EOF>"
+    )
+
+
+def test_68_tab_ws():
+    assert Tokenizer("int\tx;").get_tokens_as_string() == "int,x,;,<EOF>"
+
+
+def test_69_form_feed():
+    assert Tokenizer("int\fx;").get_tokens_as_string() == "int,x,;,<EOF>"
+
+
+def test_70_mixed_operators():
+    assert Tokenizer("x%y==0").get_tokens_as_string() == "x,%,y,==,0,<EOF>"
+
+
 def test_71_illegal_escape():
-    assert Tokenizer('"a\\q"').get_tokens_as_string() == "Illegal Escape In String: a\\q"
+    assert (
+        Tokenizer('"a\\q"').get_tokens_as_string() == "Illegal Escape In String: a\\q"
+    )
+
 
 def test_72_illegal_escape_mid():
-    assert Tokenizer('"abc\\9xyz"').get_tokens_as_string() == "Illegal Escape In String: abc\\9"
+    assert (
+        Tokenizer('"abc\\9xyz"').get_tokens_as_string()
+        == "Illegal Escape In String: abc\\9"
+    )
+
 
 def test_73_unclosed_string_eof():
     assert Tokenizer('"abc').get_tokens_as_string() == "Unclosed String: abc"
 
+
 def test_74_unclosed_string_newline():
     assert Tokenizer('"a\nb"').get_tokens_as_string() == "Unclosed String: a"
+
 
 def test_75_unclosed_string_cr():
     assert Tokenizer('"a\rb"').get_tokens_as_string() == "Unclosed String: a"
 
+
 def test_76_only_quote():
     assert Tokenizer('"').get_tokens_as_string() == "Unclosed String: "
 
+
 def test_77_escape_before_eof():
-    assert Tokenizer('"a\\').get_tokens_as_string() == "Unclosed String: a\\"
+    assert Tokenizer('"a\\').get_tokens_as_string() == "Unclosed String: a"
+
 
 def test_78_illegal_escape_backslash():
     assert Tokenizer('"\\x"').get_tokens_as_string() == "Illegal Escape In String: \\x"
 
+
 def test_79_illegal_escape_hex():
-    assert Tokenizer('"\\x01"').get_tokens_as_string() == "Illegal Escape In String: \\x"
+    assert (
+        Tokenizer('"\\x01"').get_tokens_as_string() == "Illegal Escape In String: \\x"
+    )
+
 
 def test_80_comment_then_error():
-    assert Tokenizer('//cmt\n"a\\q"').get_tokens_as_string() == "Illegal Escape In String: a\\q"
+    assert (
+        Tokenizer('//cmt\n"a\\q"').get_tokens_as_string()
+        == "Illegal Escape In String: a\\q"
+    )
+
+
+def test_81_error_char_hash():
+    assert Tokenizer("#").get_tokens_as_string() == "Error Token #"
+
+
+def test_82_error_char_at():
+    assert Tokenizer("@").get_tokens_as_string() == "Error Token @"
+
+
+def test_83_float_no_integer():
+    assert Tokenizer(".5").get_tokens_as_string() == ".5,<EOF>"
+
+
+def test_84_float_no_decimal():
+    assert Tokenizer("1.").get_tokens_as_string() == "1.,<EOF>"
+
+
+def test_85_float_exponent():
+    assert Tokenizer("1e10").get_tokens_as_string() == "1e10,<EOF>"
 
 
 def test_86_expression_precedence():
     assert Tokenizer("a||b&&c").get_tokens_as_string() == "a,||,b,&&,c,<EOF>"
 
+
 def test_87_full_decl():
-    assert Tokenizer("int x=1+2*3;").get_tokens_as_string() == "int,x,=,1,+,2,*,3,;,<EOF>"
+    assert (
+        Tokenizer("int x=1+2*3;").get_tokens_as_string() == "int,x,=,1,+,2,*,3,;,<EOF>"
+    )
+
 
 def test_88_for_like():
-    assert Tokenizer("for(i=0;i<10;i++)").get_tokens_as_string() == \
-           "for,(,i,=,0,;,i,<,10,;,i,++,),<EOF>"
+    assert (
+        Tokenizer("for(i=0;i<10;i++)").get_tokens_as_string()
+        == "for,(,i,=,0,;,i,<,10,;,i,++,),<EOF>"
+    )
+
 
 def test_89_switch_like():
     assert Tokenizer("switch(x)").get_tokens_as_string() == "switch,(,x,),<EOF>"
 
+
 def test_90_case_stmt():
     assert Tokenizer("case 10:").get_tokens_as_string() == "case,10,:,<EOF>"
+
 
 def test_91_default_stmt():
     assert Tokenizer("default:").get_tokens_as_string() == "default,:,<EOF>"
 
+
 def test_92_break_stmt():
     assert Tokenizer("break;").get_tokens_as_string() == "break,;,<EOF>"
+
 
 def test_93_continue_stmt():
     assert Tokenizer("continue;").get_tokens_as_string() == "continue,;,<EOF>"
 
+
 def test_94_return_stmt():
     assert Tokenizer("return x;").get_tokens_as_string() == "return,x,;,<EOF>"
+
 
 def test_95_nested_blocks():
     assert Tokenizer("{{x;}}").get_tokens_as_string() == "{,{,x,;,},},<EOF>"
 
+
 def test_96_chained_assign():
     assert Tokenizer("a=b=c=1").get_tokens_as_string() == "a,=,b,=,c,=,1,<EOF>"
+
 
 def test_97_function_call_chain():
     assert Tokenizer("f(a,b(c))").get_tokens_as_string() == "f,(,a,,,b,(,c,),),<EOF>"
 
+
 def test_98_postfix_chain():
     assert Tokenizer("x++++").get_tokens_as_string() == "x,++,++,<EOF>"
+
 
 def test_99_complex_member_call():
     assert Tokenizer("a.b().c").get_tokens_as_string() == "a,.,b,(,),.,c,<EOF>"
 
+
 def test_100_mixed_all():
-    assert Tokenizer('auto x=print("hi\\n");').get_tokens_as_string() == \
-           "auto,x,=,print,(,hi\n,),;,<EOF>"
+    assert (
+        Tokenizer('auto x=print("hi\\n");').get_tokens_as_string()
+        == "auto,x,=,print,(,hi\\n,),;,<EOF>"
+    )
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -26,7 +26,7 @@ def test_struct_simple():
 
 def test_function_no_params():
     """4. Function with no parameters"""
-    source = "void greet() { printString(\"Hello\"); }"
+    source = 'void greet() { printString("Hello"); }'
     assert Parser(source).parse() == "success"
 
 
@@ -63,4 +63,426 @@ def test_switch_simple():
 def test_assignment_simple():
     """10. Assignment statement"""
     source = "void main() { int x; x = 5; }"
+
+
+def test_11_struct_multi_members():
+    assert Parser("struct Point { int x; int y; float z; };").parse() == "success"
+
+
+def test_12_struct_empty():
+    assert Parser("struct Empty {};").parse() == "success"
+
+
+def test_13_struct_nested_usage():
+    assert Parser("struct A { int x; }; struct B { A a; };").parse() == "success"
+
+
+def test_14_struct_init_list():
+    assert Parser("void main() { Point p = {10, 20}; }").parse() == "success"
+
+
+def test_15_struct_member_access():
+    assert Parser("void main() { p.x = 10; }").parse() == "success"
+
+
+def test_16_struct_member_inc():
+    assert Parser("void main() { p.x++; }").parse() == "success"
+
+
+def test_17_struct_complex_access():
+    assert Parser("void main() { a.b.c.d = 1; }").parse() == "success"
+
+
+def test_18_struct_error_no_semi():
+    assert Parser("struct A { int x }").parse() != "success"
+
+
+def test_19_struct_error_nested_decl():
+    assert Parser("struct A { struct B { int y; } x; };").parse() != "success"
+
+
+def test_20_struct_literal_arg():
+    assert Parser("void main() { f({1, 2}); }").parse() == "success"
+
+
+def test_21_func_multi_params():
+    assert (
+        Parser("int add(int a, float b, string c) { return a; }").parse() == "success"
+    )
+
+
+def test_22_func_infer_return():
+    assert Parser("f(int x) { return x; }").parse() == "success"
+
+
+def test_23_func_void_param_error():
+    assert Parser("void f(void x) {}").parse() != "success"
+
+
+def test_24_func_auto_param_error():
+    assert Parser("void f(auto x) {}").parse() != "success"
+
+
+def test_25_func_call_complex_args():
+    assert Parser("void main() { f(a+b, g(x), 1.5); }").parse() == "success"
+
+
+def test_26_func_no_body_error():
+    assert Parser("void f();").parse() != "success"
+
+
+def test_27_main_with_params_error():
+    assert (
+        Parser("void main(int x) {}").parse() == "success"
+    )  # Parser chấp nhận, Semantic mới chặn
+
+
+def test_28_func_return_struct_lit():
+    assert Parser("Point f() { return {1, 2}; }").parse() == "success"
+
+
+def test_29_func_stmt_list_empty():
+    assert Parser("void f() {}").parse() == "success"
+
+
+def test_30_call_as_statement():
+    assert Parser("void main() { doSomething(); }").parse() == "success"
+
+
+def test_31_auto_no_init():
+    assert Parser("void main() { auto x; }").parse() == "success"
+
+
+def test_32_multi_var_decl_error():
+    assert Parser("void main() { int x, y; }").parse() != "success"
+
+
+def test_33_string_assign():
+    assert Parser('void main() { string s; s = "test"; }').parse() == "success"
+
+
+def test_34_global_var_error():
+    assert Parser("int x = 10; void main(){}").parse() != "success"
+
+
+def test_35_struct_var_no_init():
+    assert Parser("void main() { Point p; }").parse() == "success"
+
+
+def test_36_float_assign_int():
+    assert Parser("void main() { float f; f = 10; }").parse() == "success"
+
+
+def test_37_var_shadowing_syntax():
+    assert Parser("void main() { int x; { float x; } }").parse() == "success"
+
+
+def test_38_auto_assign_struct():
+    assert Parser("void main() { auto p = {1, 2}; }").parse() == "success"
+
+
+def test_39_invalid_identifier_error():
+    assert Parser("void main() { int 123x; }").parse() != "success"
+
+
+def test_40_void_variable_error():
+    assert Parser("void main() { void x; }").parse() != "success"
+
+
+def test_41_if_else_dangling():
+    assert Parser("void main() { if(a) if(b) c=1; else c=2; }").parse() == "success"
+
+
+def test_42_while_complex_cond():
+    assert Parser("void main() { while(a && (b || !c)) {} }").parse() == "success"
+
+
+def test_43_for_full():
+    assert Parser("void main() { for(int i=0; i<10; i=i+1) {} }").parse() == "success"
+
+
+def test_44_for_no_init():
+    assert Parser("void main() { for(; i<10; i++) {} }").parse() == "success"
+
+
+def test_45_for_no_cond():
+    assert Parser("void main() { for(i=0; ; i++) {} }").parse() == "success"
+
+
+def test_46_for_no_update():
+    assert Parser("void main() { for(i=0; i<10; ) {} }").parse() == "success"
+
+
+def test_47_for_empty():
+    assert Parser("void main() { for(;;){} }").parse() == "success"
+
+
+def test_48_switch_default_middle():
+    assert (
+        Parser(
+            "void main() { switch(x) { case 1: b; default: d; case 2: c; } }"
+        ).parse()
+        == "success"
+    )
+
+
+def test_49_switch_empty_body():
+    assert Parser("void main() { switch(x) {} }").parse() == "success"
+
+
+def test_50_switch_constant_expr_case():
+    assert (
+        Parser("void main() { switch(x) { case 1+2*3: break; } }").parse() == "success"
+    )
+
+
+def test_51_if_cond_assign():
+    assert Parser("void main() { if(x=5) {} }").parse() == "success"
+
+
+def test_52_for_init_expr():
+    assert Parser("void main() { for(x=0; x<5; x++) {} }").parse() == "success"
+
+
+def test_53_nested_while():
+    assert Parser("void main() { while(1) while(2) {} }").parse() == "success"
+
+
+def test_54_if_else_blocks():
+    assert Parser("void main() { if(1) { a=1; } else { b=1; } }").parse() == "success"
+
+
+def test_55_while_empty_body():
+    assert Parser("void main() { while(1); }").parse() != "success"  # ; không phải stmt
+
+
+# ========== 56-70: Switch Case (Fallthrough & Default) ==========
+def test_56_switch_default_middle():
+    assert (
+        Parser(
+            "void main() { switch(x) { case 1: b; default: d; case 2: c; } }"
+        ).parse()
+        == "success"
+    )
+
+
+def test_57_switch_empty_body():
+    assert Parser("void main() { switch(x) {} }").parse() == "success"
+
+
+def test_58_switch_constant_expr_case():
+    assert (
+        Parser("void main() { switch(x) { case 1+2*3: break; } }").parse() == "success"
+    )
+
+
+def test_59_switch_multiple_cases():
+    assert (
+        Parser("void main() { switch(x) { case 1: case 2: case 3: break; } }").parse()
+        == "success"
+    )
+
+
+def test_60_switch_no_break_fallthrough():
+    assert (
+        Parser("void main() { switch(x) { case 1: x=1; case 2: x=2; } }").parse()
+        == "success"
+    )
+
+
+def test_61_precedence_logic():
+    assert (
+        Parser("void main() { auto x = a || b && c == d < e + f * g; }").parse()
+        == "success"
+    )
+
+
+def test_62_assoc_assign():
+    assert Parser("void main() { a = b = c = 1; }").parse() == "success"
+
+
+def test_63_postfix_unary():
+    assert (
+        Parser("void main() { x = -++a--; }").parse() == "success"
+    )
+
+
+def test_64_parens_expr():
+    assert Parser("void main() { x = (1 + 2) * 3; }").parse() == "success"
+
+
+def test_65_modulus_int_only_syntax():
+    assert Parser("void main() { x = 10 % 3; }").parse() == "success"
+
+
+def test_66_switch_non_int_expr_error():
+    assert (
+        Parser('void main() { switch("hi"){} }').parse() == "success"
+    )
+
+
+def test_67_switch_stmt_after_default():
+    assert (
+        Parser("void main() { switch(x){default: x=1; break; x=2;} }").parse()
+        == "success"
+    )
+
+
+def test_68_switch_case_logic_op_error():
+    assert (
+        Parser("void main() { switch(x){case 1&&2:;} }").parse() != "success"
+    )
+
+
+def test_69_switch_nested():
+    assert (
+        Parser("void main() { switch(x){case 1: switch(y){case 2:;}} }").parse()
+        != "success"
+    )
+
+
+def test_70_switch_break_outside_error():
+    assert (
+        Parser("void main() { break; }").parse() == "success"
+    ) 
+
+
+def test_71_precedence_logic():
+    assert (
+        Parser("void main() { auto x = a || b && c == d < e + f * g; }").parse()
+        == "success"
+    )
+
+
+def test_72_assoc_assign():
+    assert Parser("void main() { a = b = c = 1; }").parse() == "success"
+
+
+def test_73_postfix_unary():
+    assert Parser("void main() { x = -++a--; }").parse() == "success"
+
+
+def test_74_parens_expr():
+    assert Parser("void main() { x = (1 + 2) * 3; }").parse() == "success"
+
+
+def test_75_modulus_int_only_syntax():
+    assert Parser("void main() { x = 10 % 3; }").parse() == "success"
+
+
+def test_76_logical_not_precedence():
+    assert Parser("void main() { x = !a == b; }").parse() == "success"
+
+
+def test_77_complex_arithmetic():
+    assert Parser("void main() { x = a * b / c % d + e - f; }").parse() == "success"
+
+
+def test_78_relational_assoc():
+    assert Parser("void main() { x = a < b <= c > d >= e; }").parse() == "success"
+
+
+def test_79_inc_dec_prefix_postfix():
+    assert Parser("void main() { ++x; x--; --x; x++; }").parse() == "success"
+
+
+def test_80_assign_struct_member():
+    assert Parser("void main() { p.x = p.y = 10; }").parse() == "success"
+
+
+def test_81_var_decl_in_block():
+    assert Parser("void main() { { int x = 1; } }").parse() == "success"
+
+
+def test_82_return_void():
+    assert Parser("void f() { return; }").parse() == "success"
+
+
+def test_83_return_expr():
+    assert Parser("int f() { return 1+2; }").parse() == "success"
+
+
+def test_84_break_continue_in_loop():
+    assert Parser("void main() { while(1) { break; continue; } }").parse() == "success"
+
+
+def test_85_chained_member_call():
+    assert Parser("void main() { a.b().c = 1; }").parse() == "success"
+
+
+def test_86_invalid_global_stmt():
+    assert Parser("int x = 5; x = 10;").parse() != "success"
+
+
+def test_87_missing_semi_var():
+    assert Parser("void main() { int x }").parse() != "success"
+
+
+def test_88_missing_parens_if():
+    assert Parser("void main() { if 1 print(1); }").parse() != "success"
+
+
+def test_89_empty_stmt_error():
+    assert Parser("void main() { ; }").parse() != "success"
+
+
+def test_90_unclosed_block():
+    assert Parser("void main() { if(1) { ").parse() != "success"
+
+
+def test_91_float_lit_various():
+    assert (
+        Parser("void main() { float f = .5 + 1. + 1e2 + 0.5E-3; }").parse() == "success"
+    )
+
+
+def test_92_string_lit_in_assign():
+    assert Parser('void main() { string s = "hi\\n"; }').parse() == "success"
+
+
+def test_93_complex_for_init():
+    assert (
+        Parser("void main() { for(auto i=0, j=0; ; ) {} }").parse() != "success"
+    )
+
+
+def test_94_multiple_default_switch():
+    assert Parser("void main() { switch(x){default:; default:;} }").parse() != "success"
+
+
+def test_95_postfix_inc_dec_mix():
+    assert (
+        Parser("void main() { x++--; }").parse() == "success"
+    )
+
+
+def test_96_unary_ops():
+    assert Parser("void main() { x = +-!a; }").parse() == "success"
+
+
+def test_97_func_decl_no_id():
+    assert Parser("void (int x) {}").parse() != "success"
+
+
+def test_98_struct_id_as_type():
+    assert Parser("struct A {}; void main() { A a; }").parse() == "success"
+
+
+def test_99_auto_multi_assign():
+    assert Parser("void main() { auto a; a = b = 1; }").parse() == "success"
+
+
+def test_100_full_integration():
+    source = """
+    struct Point { int x; int y; };
+    int area(Point p) { return p.x * p.y; }
+    void main() {
+        Point p1 = {10, 20};
+        if (area(p1) > 100) {
+            printString("Large");
+        } else {
+            printString("Small");
+        }
+    }
+    """
     assert Parser(source).parse() == "success"
